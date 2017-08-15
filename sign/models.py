77ae7cad7dea3770b339f1f476cb18c9c470c2a6
zuiniao123 <932669040@qq.com>
2017-08-15 09:49:02 +0800

    second commit

--- sign/models.py
+++ sign/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 # 发布会表
 
+
 class Event(models.Model):
 	name = models.CharField(max_length=100)
 	limit = models.IntegerField()
@@ -10,10 +11,12 @@ class Event(models.Model):
 	address = models.CharField(max_length=200)
 	start_time = models.DateField("events time")
 	create_time = models.DateField(auto_now=True)
-	
+
+
 	def __str__(self):
 		return self.name
 	pass
+
 
 class Guest(models.Model):
 	event = models.ForeignKey(Event)
